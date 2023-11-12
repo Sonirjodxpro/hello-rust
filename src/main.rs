1c85a27b0875e6e180486ca057efd40a5baf0528
sunjinxiang <345281398@qq.com>
2023-11-12 22:51:59 +0800

    fix: 修复问题

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 fn main() {
-    let a = 10;
-    let b: i32 = 20;
+    let guess: i32 = "42".parse().expect("Not a number!");
+    println!("guess = {}", guess);
 
-    let mut c = 30i32;
-    c = 40;
-    let d = 30_i32;
+    // let a: u8 = 255;
 
-    let e = add(add(a, b), add(c, d));
+    //使用 wrapping_* 方法在所有模式下都按照补码循环溢出规则处理，例如 wrapping_add
+    // let b = a.wrapping_add(30);
+    // println!("a = {}, b = {}", a, val);
 
-    println!("a +b + c + d = {}", e);
-}
-fn add(i: i32, j: i32) -> i32 {
-    i + j
+    //如果使用 checked_* 方法时发生溢出，则返回 None 值
+    // let b = a.checked_add(30);
+    // match b {
+    //     Some(val) => println!("a = {}, b = {}", a, val),
+    //     None => println!("Overflow occurred"),
+    // }
+
+    //使用 overflowing_* 方法返回该值和一个指示是否存在溢出的布尔值
+    // let b = a.overflowing_add(30);
+    // println!("a = {}, b = {},b.1 = {}", a, b.0, b.1);
+
+    // 使用 saturating_* 方法使值达到最小值或最大值
+
+    // let b = a.saturating_add(30);
+    // println!("a = {}, b = {}", a, b);
+
+    assert!(0.1 + 0.2 == 0.3);
 }
